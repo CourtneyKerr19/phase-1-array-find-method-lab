09fadb8211dc506de496331b2db51c4e1a79d4c2
Courtney Kerr <courtneykerr219@gmail.com>
2024-02-11 00:20:51 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(records) {
+    const winningRecord = records.find(record => record.result === "W");
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  } 
+  console.log(superbowlWin(record)); // Output: "2015"
+
